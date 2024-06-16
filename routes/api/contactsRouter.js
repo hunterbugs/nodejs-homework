760f7452f87e7ded68b2ecb8a6ddac6923a4066f
hunterbugs <79522220+hunterbugs@users.noreply.github.com>
2024-06-16 22:29:02 +0800

    update

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -2,27 +2,43 @@ import express from "express";
 import { ctrlWrapper } from "../../helpers/ctrlWrapper.js";
 // prettier-ignore
 import { addContact, deleteContactById, getAllContacts, getContactById, updateContactById, updateStatusContact } from "../../controllers/contactsController.js";
+import { authenticateToken } from "../../middlewares/authenticateToken.js";
 
 const router = express.Router();
 
 /* GET: // http://localhost:3000/api/contacts */
-router.get("/", ctrlWrapper(getAllContacts));
+router.get("/", authenticateToken, ctrlWrapper(getAllContacts));
 
 /* GET: // http://localhost:3000/api/contacts/:contactId */
-router.get("/:contactId", ctrlWrapper(getContactById));
+router.get("/:contactId", authenticateToken, ctrlWrapper(getContactById));
 
-router.post("/", ctrlWrapper(addContact));
+/* POST: // http://localhost:3000/api/contacts/ 
+{
+    "name": "Marvin Pacis",
+    "email": "marvinpacis@example.com",
+    "phone": "(639) 840-6611"
+} 
+*/
+router.post("/", authenticateToken, ctrlWrapper(addContact));
 
 /* DELETE: // http://localhost:3000/api/contacts/:contactId */
-router.delete("/:contactId", ctrlWrapper(deleteContactById));
+router.delete("/:contactId", authenticateToken, ctrlWrapper(deleteContactById));
 
-router.put("/:contactId", ctrlWrapper(updateContactById));
+/* PUT: // http://localhost:3000/api/contacts/:contactId 
+{
+    "name": "Joanna Shaw",
+    "email": "joannashaw@example.com",
+    "phone": "(639) 777-8819"
+} 
+*/
+router.put("/:contactId", authenticateToken, ctrlWrapper(updateContactById));
 
 /* PATCH: // http://localhost:3000/api/contacts/:contactId/favorite
 {
     "favorite": true,
 }
 */
-router.patch("/:contactId/favorite", ctrlWrapper(updateStatusContact));
+// prettier-ignore
+router.patch("/:contactId/favorite", authenticateToken, ctrlWrapper(updateStatusContact));
 
 export { router };
